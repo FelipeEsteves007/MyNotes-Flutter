b6c6adcd10f60205d9524c8976e88e8b90a2d2fa
devFelipe <felipeestevesdepaula@gmail.com>
2026-08-04 21:59:45 -0300

    add services, exceptions and AuthUser

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,14 @@
+// login exceptions
+class InvalidCredentialsException implements Exception {}
+
+// register exceptions
+class WeakpeakException implements Exception {}
+
+class EmailAlreadyInUseExcepeption implements Exception {}
+
+class InvalidEmailException implements Exception {}
+
+// generic exceptions
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
